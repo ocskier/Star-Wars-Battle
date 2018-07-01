10dad5bba269ec61bc91932a8a3323c19def4a44
Pack Fan 1031 <ocskier@gmail.com>
2018-07-01 01:04:40 -0400

    final draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,44 @@
 // Global Variables for player objects, booleans, and current player and enemy fighters
-var DarthMaul = createPlyrAttr("Darth Maul");
-var Rey = createPlyrAttr("Rey");
-var Emperor = createPlyrAttr("The Emperor");
-var Chewbacca= createPlyrAttr("Chewbacca");
+var DarthMaul = createPlyrAttr("darthMaul");
+var Rey = createPlyrAttr("rey");
+var Emperor = createPlyrAttr("emperorPalp");
+var Chewbacca= createPlyrAttr("chewbacca");
+var picDarth = '<img class="card-img-top" src="assets/images/Darth-Maul-02-icon.png" alt="Card image cap">';
+var picRey = '<img class="card-img-top" src="assets/images/main-thumb-t-1785517-200-boztfctkwxbzmfjwurneimbimaluhaxk.jpeg" alt="Card image cap">';
 var enemyLeft = 3;
-var pickedfirst = false;
-var pickedsecond = false;
-var pickedthird = false;
-var pickedLast = false;
-var defDead = false;
-var Plyr1 = {};
-var Plyr2 = {};
+var pickedfirst, pickedsecond, pickedthird, pickedLast, defDead = false;
+var Plyr1, Plyr2 = {};
 
 // Printing to the console all the attributes of the four characters
 console.log(DarthMaul);
 console.log(Rey);
 console.log(Emperor);
 console.log(Chewbacca);
-
-// Generating the random attributes for all 4 players
-printPlrHP();
-
-// Starting the game prompt and a slight delay for page to load
-setTimeout(function () {
-    alert("Pick a player for yourself!");
-}, 1000);
+$("#battle-txt").append("<p>Pick a Player for Yourself!</p>");
 
 // A f(x) for generating a characters random attributes for hp, current, base, and counter attack powers
 function createPlyrAttr (Name) {
     var charname = Name;
     var hp = Math.floor(Math.random()*100)+100;
-    var num2 = Math.floor(Math.random()*10)+10;
     var num3 = Math.floor(Math.random()*10)+15;
-    var beenChosen = false;
-    var thisPlr = {Name: charname, charHP: hp,totalAttackPwr: num2, baseAttackPwr: num2, counterAttackPwr: num3};
+    var num2 = Math.floor(Math.random()*10)+10;
+    var thisPlr = {Name: charname, charHP: hp,totalAttackPwr: num2, baseAttackPwr: num2, counterAttackPwr: num3, 
+        printPlrHP: function() {$('#' + this.Name +" .mycard ul li").text(this.charHP + " HP");}};
+    thisPlr.printPlrHP();
     return thisPlr;
 }
 
-// A f(x) for printing initial hp for each player card on screen
-function printPlrHP () {
-    $("#darthMaul .mycard ul li").text(DarthMaul.charHP + " HP");
-    $("#rey .mycard ul li").text(Rey.charHP + " HP");
-    $("#emperorPalp .mycard ul li").text(Emperor.charHP + " HP");
-    $("#chewbacca .mycard ul li").text(Chewbacca.charHP + " HP"); 
-}
-
-// A f(x) to assign the players attribute holding object to his card when clicked on
-function getPlayer () {
-    if (playerClick == "darthMaul") {
-        var player = DarthMaul;
-    }
-    else if (playerClick == "rey") {
-        var player = Rey;
-    }
-    else if (playerClick == "emperorPalp") {
-        var player = Emperor;
-    }
-    else if (playerClick == "chewbacca") {
-        var player = Chewbacca;
-    }
-    console.log(player);
-    return player;
-}
-
-// A f(x) to remove the pic of the character from the holding row when picked
-function removePic (pick) {
-    $("#" + pick).remove();
-}
-
 // A f(x) for assigning a character and his card to the player area and asking for next choice 
-function movePlayer () {
-    Plyr1 = getPlayer();
+function movePlayer (playerchar) {
+    Plyr1 = playerchar;
     pickedfirst = true;
-    $("#battle-txt").append("<p>Pick an enemy player!</p>");
+    $("#battle-txt").empty().append("<p>Pick an enemy player!</p>");
 }
 
 // A f(x) for assigning a character and his card to the enemy area and asking to attack
-function moveNextEnemy () {
-    Plyr2 = getPlayer ();
-    $("#battle-txt").empty();
-    $("#battle-txt").append("<p>Press Attack!</p>");
+function moveNextEnemy (playerchar) {
+    Plyr2 = playerchar;
+    $("#battle-txt").empty().append("<p>Press Attack!</p>");
 }
 
 // A f(x) for adjusting the hp of player after attack
@@ -111,134 +70,59 @@ function wasAttacked (defPlr,offPwr){
         return thisPlr;
 }
 
-// Click functions for each character for moving their respective cards based on click order and battle status
-
-$("#darthMaul").on("click",function() {
-    playerClick = "darthMaul";
-    newOffDiv = $('<div class="card w-100 myoffcard" style="width: 2rem;"><img class="card-img-top" src="assets/images/Darth-Maul-02-icon.png" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Darth Maul</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ DarthMaul.charHP + "HP</li></ul></div>");
-    newDefDiv = $('<div class="card w-100 mycard" style="width: 2rem;"><img class="card-img-top" src="assets/images/Darth-Maul-02-icon.png" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Darth Maul</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ DarthMaul.charHP + "HP</li></ul></div>");
-
+// A f(x) for deciding actions for player cards based on click order and battle status
+function choosePic (picName,OffDiv,DefDiv) {
     if (!pickedfirst) {
-        removePic(playerClick);
-        $("#playerArea").append(newOffDiv);
-        movePlayer();
+        $("#"+picName.Name).remove();
+        $("#playerArea").append(OffDiv);
+        movePlayer(picName);
     } 
     else if (!pickedsecond) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
+        $("#"+picName.Name).remove();
+        $("#defenderArea").append(DefDiv);
+        moveNextEnemy(picName);
         pickedsecond = true;    
     }
     else if (!pickedthird && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
+        $("#"+picName.Name).remove();
+        $("#defenderArea").append(DefDiv);
+        moveNextEnemy(picName);
         defDead = false;
         pickedthird = true;
     }
     else if (!pickedLast && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
+        $("#"+picName.Name).remove();
+        $("#defenderArea").append(DefDiv);
+        moveNextEnemy(picName);
         defDead = false;
         pickedLast = true;
     }
+}
+
+// Click functions for each character for moving their respective cards
+
+$("#darthMaul").on("click",function() {
+    newOffDiv = $('<div class="card w-100 myoffcard" style="width: 2rem;">'+picDarth+'<div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Darth Maul</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ DarthMaul.charHP + "HP</li></ul></div>");
+    newDefDiv = $('<div class="card w-100 mycard" style="width: 2rem;"><img class="card-img-top" src="assets/images/Darth-Maul-02-icon.png" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Darth Maul</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ DarthMaul.charHP + "HP</li></ul></div>");
+    choosePic(DarthMaul,newOffDiv,newDefDiv);
 });
 
 $("#rey").on("click",function() {
-    playerClick = "rey";
     newOffDiv = $('<div class="card w-100 myoffcard" style="width: 2rem;"><img class="card-img-top" src="assets/images/main-thumb-t-1785517-200-boztfctkwxbzmfjwurneimbimaluhaxk.jpeg" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Rey</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ Rey.charHP + "HP</li></ul></div>");
     newDefDiv = $('<div class="card w-100 mycard" style="width: 2rem;"><img class="card-img-top" src="assets/images/main-thumb-t-1785517-200-boztfctkwxbzmfjwurneimbimaluhaxk.jpeg" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Rey</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ Rey.charHP + "HP</li></ul></div>");
-
-    if (!pickedfirst) {
-        removePic(playerClick);
-        $("#playerArea").append(newOffDiv);
-        movePlayer();
-    } 
-    else if (!pickedsecond) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        pickedsecond = true;
-    }
-    else if (!pickedthird && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        defDead = false;
-        pickedthird = true;
-    }
-    else if (!pickedLast && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        defDead = false;
-        pickedLast = true;
-    }
+    choosePic(Rey,newOffDiv,newDefDiv);
 });
 
 $("#emperorPalp").on("click",function() {
-    playerClick = "emperorPalp";
     newOffDiv = $('<div class="card w-100 myoffcard" style="width: 2rem;"><img class="card-img-top" src="assets/images/rendition1.img.jpg" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Emperor</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">' + Emperor.charHP + "HP</li></ul></div>");
     newDefDiv = $('<div class="card w-100 mycard" style="width: 2rem;"><img class="card-img-top" src="assets/images/rendition1.img.jpg" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Emperor</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">' + Emperor.charHP + "HP</li></ul></div>");
-    
-    if (!pickedfirst) {
-        removePic(playerClick);
-        $("#playerArea").append(newOffDiv);
-        movePlayer();
-    }
-    else if (!pickedsecond) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        pickedsecond = true;
-    }
-    else if (!pickedthird && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        defDead = false;
-        pickedthird = true;
-    }
-    else if (!pickedLast && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        defDead = false;
-        pickedLast = true;
-    }
+    choosePic(Emperor,newOffDiv,newDefDiv);
 });
 
 $("#chewbacca").on("click",function() {
-    playerClick = "chewbacca";
     newOffDiv = $('<div class="card w-100 myoffcard" style="width: 2rem;"><img class="card-img-top" src="assets/images/ChewbaccaROTS.jpg" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Chewy</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ Chewbacca.charHP + "HP</li></ul></div>");
     newDefDiv = $('<div class="card w-100 mycard" style="width: 2rem;"><img class="card-img-top" src="assets/images/ChewbaccaROTS.jpg" alt="Card image cap"><div class="card-body" style="padding: 5px 0 0 0;"><h5 class="card-title">Chewy</h5><p class="card-text"></p></div><ul class="list-group list-group-flush"><li class="list-group-item">'+ Chewbacca.charHP + "HP</li></ul></div>");
-
-    if (!pickedfirst) {
-        removePic(playerClick);
-        $("#playerArea").append(newOffDiv);
-        movePlayer();
-    } 
-    else if (!pickedsecond) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        pickedsecond = true;
-    }
-    else if (!pickedthird && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        defDead = false;
-        pickedthird = true;
-    }
-    else if (!pickedLast && defDead) {
-        removePic(playerClick);
-        $("#defenderArea").append(newDefDiv);
-        moveNextEnemy();
-        defDead = false;
-        pickedLast = true;
-    }
+    choosePic(Chewbacca,newOffDiv,newDefDiv);
 });
 
 // Attack click function for adjusting hp, losing and winning game, dealing with attack and no enemy, etc.
@@ -257,8 +141,7 @@ $(".attackbtn").on("click", function() {
             else if (Plyr2.charHP <= 0) {
                 enemyLeft--;
                 $("#defenderArea").empty();
-                $("#battle-txt").empty();
-                $("#battle-txt").append("<p>You killed " + Plyr2.Name + "!</p>");
+                $("#battle-txt").empty().append("<p>You killed " + Plyr2.Name + "!</p>");
                 defDead = true;
                 if (enemyLeft > 0) {
                     $("#battle-txt").append("<p>Pick another enemy!</p>");
@@ -272,16 +155,10 @@ $(".attackbtn").on("click", function() {
             } 
         }
         else if (!pickedfirst) { 
-            alert("No player picked!");
+            $("#battle-txt").empty().append("<p>No player picked yet!</p>");
         }
         else if (!pickedsecond) {
-            alert("You haven't picked an enemy yet!");
+            $("#battle-txt").empty().append("<p>You haven't picked an enemy yet!</p>");
         }
-        else {
-            alert("Pick another enemy!");
-        }
-    }
-    else {
-        alert("Everybodys dead!!!!");
     }
 });
